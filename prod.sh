1b23e2fae81dbd71ef35ec5f9f84a3829ac5ce7f
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-07-09 23:25:37 +0200

    Update prod.sh

--- prod.sh
+++ prod.sh
@@ -177,7 +177,7 @@ daemon_setup() {
   if ! id "fluxuser" &>/dev/null; then
     _task "Create user fluxuser"
     _cmd "sudo useradd -p '' -r -s /bin/bash -m fluxuser"
-    _cmd "fluxuser ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/dont-prompt-fluxuser-for-sudo-password"
+    _cmd "echo \"fluxuser ALL=(ALL:ALL) NOPASSWD: ALL\" | sudo tee \"/etc/sudoers.d/dont-prompt-fluxuser-for-sudo-password\" > /dev/null"
   fi
   echo -e "${YELLOW}==============================================================================================================${RESTORE}"
   _task "Download the Fluxcore executable"
